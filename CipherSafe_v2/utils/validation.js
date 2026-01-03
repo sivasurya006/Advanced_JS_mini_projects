e0d72e6bf0afc708cd0e4aad6efd794f739e482d
sivasurya006 <titansurya88@gmail.com>
2026-01-03 22:48:15 +0530

    Remove all tracked files and reset tracking

--- CipherSafe_v2/utils/validation.js
+++
@@ -1,7 +0,0 @@
-export function isValidSignupCredentials(name, email, password) {
-    return name && password && name.trim().length > 0 && email.trim().length > 0 && password.trim().length > 0;
-}
-
-export function isValidSigninCredentials(email, password) {
-    return email && password && email.trim().length > 0 && password.trim().length > 0;
-}
